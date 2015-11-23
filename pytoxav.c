d35233bb9b43b42ce7d834bd01a49ffe1980c6fb
Anton Batenev <antonbatenev@yandex.ru>
2015-11-23 04:43:19 +0300

    Add rgb_to_yuv420 / yuv420_to_rgb

--- pytoxav.c
+++ pytoxav.c
@@ -49,7 +49,52 @@ inline static uint8_t rgb_to_v(int r, int g, int b)
 }
 //----------------------------------------------------------------------------------------------
 
-static void bgr_to_yuv420(uint8_t* plane_y, uint8_t* plane_u, uint8_t* plane_v, uint8_t* rgb, uint16_t width, uint16_t height)
+static void bgr_to_yuv420(uint8_t* plane_y, uint8_t* plane_u, uint8_t* plane_v, uint8_t* bgr, uint16_t width, uint16_t height)
+{
+    uint16_t x;
+    uint16_t y;
+    uint8_t* p;
+    uint8_t  b;
+    uint8_t  g;
+    uint8_t  r;
+
+    for (y = 0; y != height; y += 2) {
+        p = bgr;
+        for (x = 0; x != width; x++) {
+            b = *bgr++;
+            g = *bgr++;
+            r = *bgr++;
+
+            *plane_y++ = rgb_to_y(r, g, b);
+        }
+
+        for (x = 0; x != width / 2; x++) {
+            b = *bgr++;
+            g = *bgr++;
+            r = *bgr++;
+
+            *plane_y++ = rgb_to_y(r, g, b);
+
+            b = *bgr++;
+            g = *bgr++;
+            r = *bgr++;
+
+            *plane_y++ = rgb_to_y(r, g, b);
+
+            b = ((int)b + (int)*(bgr - 6) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
+            g = ((int)g + (int)*(bgr - 5) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
+            r = ((int)r + (int)*(bgr - 4) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
+
+            *plane_u++ = rgb_to_u(r, g, b);
+            *plane_v++ = rgb_to_v(r, g, b);
+
+            p += 3;
+        }
+    }
+}
+//----------------------------------------------------------------------------------------------
+
+static void rgb_to_yuv420(uint8_t* plane_y, uint8_t* plane_u, uint8_t* plane_v, uint8_t* rgb, uint16_t width, uint16_t height)
 {
     uint16_t x;
     uint16_t y;
@@ -61,29 +106,29 @@ static void bgr_to_yuv420(uint8_t* plane_y, uint8_t* plane_u, uint8_t* plane_v, 
     for (y = 0; y != height; y += 2) {
         p = rgb;
         for (x = 0; x != width; x++) {
-            b = *rgb++;
-            g = *rgb++;
             r = *rgb++;
+            g = *rgb++;
+            b = *rgb++;
 
             *plane_y++ = rgb_to_y(r, g, b);
         }
 
         for (x = 0; x != width / 2; x++) {
-            b = *rgb++;
-            g = *rgb++;
             r = *rgb++;
+            g = *rgb++;
+            b = *rgb++;
 
             *plane_y++ = rgb_to_y(r, g, b);
 
-            b = *rgb++;
-            g = *rgb++;
             r = *rgb++;
+            g = *rgb++;
+            b = *rgb++;
 
             *plane_y++ = rgb_to_y(r, g, b);
 
-            b = ((int)b + (int)*(rgb - 6) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
+            r = ((int)b + (int)*(rgb - 6) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
             g = ((int)g + (int)*(rgb - 5) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
-            r = ((int)r + (int)*(rgb - 4) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
+            b = ((int)r + (int)*(rgb - 4) + (int)*p + (int)*(p + 3) + 2) / 4; p++;
 
             *plane_u++ = rgb_to_u(r, g, b);
             *plane_v++ = rgb_to_v(r, g, b);
@@ -94,14 +139,41 @@ static void bgr_to_yuv420(uint8_t* plane_y, uint8_t* plane_u, uint8_t* plane_v, 
 }
 //----------------------------------------------------------------------------------------------
 
-static void yuv420_to_bgr(uint16_t width, uint16_t height, const uint8_t* y, const uint8_t* u, const uint8_t* v, unsigned int ystride, unsigned int ustride, unsigned int vstride, uint8_t* out)
+static void yuv420_to_bgr(uint16_t width, uint16_t height, const uint8_t* y, const uint8_t* u, const uint8_t* v, unsigned int ystride, unsigned int ustride, unsigned int vstride, uint8_t* bgr)
 {
     unsigned long int i;
     unsigned long int j;
 
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
-            uint8_t* point = out + 4 * ((i * width) + j);
+            uint8_t* point = bgr + 3 * ((i * width) + j);
+
+            int t_y = y[((i * ystride) + j)];
+            int t_u = u[(((i / 2) * ustride) + (j / 2))];
+            int t_v = v[(((i / 2) * vstride) + (j / 2))];
+
+            t_y = t_y < 16 ? 16 : t_y;
+
+            int b = (298 * (t_y - 16) + 516 * (t_u - 128) + 128) >> 8;
+            int g = (298 * (t_y - 16) - 100 * (t_u - 128) - 208 * (t_v - 128) + 128) >> 8;
+            int r = (298 * (t_y - 16) + 409 * (t_v - 128) + 128) >> 8;
+
+            point[0] = b > 255 ? 255 : b < 0 ? 0 : b;
+            point[1] = g > 255 ? 255 : g < 0 ? 0 : g;
+            point[2] = r > 255 ? 255 : r < 0 ? 0 : r;
+        }
+    }
+}
+//----------------------------------------------------------------------------------------------
+
+static void yuv420_to_rgb(uint16_t width, uint16_t height, const uint8_t* y, const uint8_t* u, const uint8_t* v, unsigned int ystride, unsigned int ustride, unsigned int vstride, uint8_t* rgb)
+{
+    unsigned long int i;
+    unsigned long int j;
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            uint8_t* point = rgb + 3 * ((i * width) + j);
 
             int t_y = y[((i * ystride) + j)];
             int t_u = u[(((i / 2) * ustride) + (j / 2))];
@@ -113,10 +185,9 @@ static void yuv420_to_bgr(uint16_t width, uint16_t height, const uint8_t* y, con
             int g = (298 * (t_y - 16) - 100 * (t_u - 128) - 208 * (t_v - 128) + 128) >> 8;
             int b = (298 * (t_y - 16) + 516 * (t_u - 128) + 128) >> 8;
 
-            point[2] = r > 255 ? 255 : r < 0 ? 0 : r;
+            point[0] = r > 255 ? 255 : r < 0 ? 0 : r;
             point[1] = g > 255 ? 255 : g < 0 ? 0 : g;
-            point[0] = b > 255 ? 255 : b < 0 ? 0 : b;
-            point[3] = ~0;
+            point[2] = b > 255 ? 255 : b < 0 ? 0 : b;
         }
     }
 }
@@ -144,19 +215,26 @@ static void callback_audio_receive_frame(ToxAV* av, uint32_t friend_number, cons
 {
     size_t pcm_len = sample_count * channels * 2;
 
-    // TODO:
     PyObject_CallMethod((PyObject*)self, "toxav_audio_receive_frame_cb", "I" BUF_TCS "KII", friend_number, pcm, pcm_len, sample_count, channels, sampling_rate);
 }
 //----------------------------------------------------------------------------------------------
 
 static void callback_video_receive_frame(ToxAV* av, uint32_t friend_number, uint16_t width, uint16_t height, const uint8_t* y, const uint8_t* u, const uint8_t* v, int32_t ystride, int32_t ustride, int32_t vstride, void* self)
 {
-    size_t y_len = max(width, abs(ystride)) * height;
-    size_t u_len = max(width / 2, abs(ustride)) * (height / 2);
-    size_t v_len = max(width / 2, abs(vstride)) * (height / 2);
+    uint8_t* rgb = malloc(width * height * 3);
+    if (rgb == NULL)
+        return;
 
-    // TODO:
-    PyObject_CallMethod((PyObject*)self, "toxav_video_receive_frame_cb", "III" BUF_TCS BUF_TCS BUF_TCS "III", friend_number, width, height, y, y_len, u, u_len, v, v_len, ystride, ustride, vstride);
+    // TODO: Choose bgr / rbg
+    ystride = abs(ystride);
+    ustride = abs(ustride);
+    vstride = abs(vstride);
+
+    yuv420_to_bgr(width, height, y, u, v, ystride, ustride, rgb);
+
+    PyObject_CallMethod((PyObject*)self, "toxav_video_receive_frame_cb", "III" BUF_TCS, friend_number, width, height, rgb);
+
+    free(rgb);
 }
 //----------------------------------------------------------------------------------------------
 
