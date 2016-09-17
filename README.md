c74f84797583bdf170d380080f4ade7c2ecbdceb
Anton Batenev <antonbatenev@yandex.ru>
2016-09-18 01:04:31 +0300

    Fix #9 - OS X doesn't have librt

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ Python binding for [ToxCore](https://github.com/irungentoo/toxcore).
 * [Fedora, openSUSE, CentOS](http://software.opensuse.org/download.html?project=home:antonbatenev:tox&package=python-toxcore)
 * [Arch](http://software.opensuse.org/download.html?project=home:antonbatenev:tox&package=python-toxcore), [Arch AUR](https://aur.archlinux.org/packages/python-toxcore) (see also [AUR Helpers](https://wiki.archlinux.org/index.php/AUR_Helpers))
 
+### Compile from sources
+
+```
+$ python setup.py build
+$ python setup.py install
+```
+
 ### Usage
 
 See [Echo Bot Example](https://github.com/abbat/pytoxcore/tree/master/examples).
--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+import sys
 from distutils.core import setup, Extension
+
+libraries = ["toxcore", "toxav", "toxdns", "sodium", "vpx", "opus"]
+
+if sys.platform != "darwin":
+    libraries.append("rt")
 
 setup(
     name         = "pytoxcore",
     version      = "0.2.3",
-    description  = 'Python binding for ToxCore',
-    author       = 'Anton Batenev',
-    author_email = 'antonbatenev@yandex.ru',
-    url          = 'http://github.com/abbat/pytoxcore',
-    license      = 'GPL',
+    description  = "Python binding for ToxCore",
+    author       = "Anton Batenev",
+    author_email = "antonbatenev@yandex.ru",
+    url          = "http://github.com/abbat/pytoxcore",
+    license      = "GPL",
     ext_modules  = [
         Extension(
             "pytoxcore",
@@ -16,7 +22,7 @@ setup(
             include_dirs       = ["/usr/tox/include"],
             library_dirs       = ["/usr/tox/lib"],
             extra_compile_args = ["-Wall", "-Werror", "-Wno-declaration-after-statement"],
-            libraries          = ["toxcore", "toxav", "toxdns", "sodium", "vpx", "opus", "rt"]
+            libraries          = libraries
         )
     ]
 )
